3b19cbfb89e6ba83c7a97d3704f27283a8bd02b5
olimiloolimilo <olimiloolimilo@protonmail.com>
2021-08-02 21:00:33 +0700

    On branch unittest
    	new file:   src/SomeMethodsTest.java     // template for basic j-unit test of SomeMethods Class.
    
    	new file:   src/SomeMethodsTest.java~

---
+++ src/SomeMethodsTest.java
@@ -0,0 +1,23 @@
+//tests for lab 1 code
+import org.junit.Assertions.*;
+import org.junit.Test;
+
+
+
+public class SomeMethodsTest{
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+	SomeMethods tester = new SomeMethods(); // MyClass is tested
+
+	
+	// assert statements
+	/*
+	assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+	assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+	assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+	*/
+    }
+
+
+}
